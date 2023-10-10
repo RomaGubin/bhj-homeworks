ab2cce0ccb64517557275cb64c6467d0c7bba53f
RomaGubin <133153869+RomaGubin@users.noreply.github.com>
2023-10-10 23:28:56 +0500

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -15,7 +15,7 @@ class Game {
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
-
+// после вебинара решил задачу
   registerEvents() {
 
     let currentPosition = 0;
