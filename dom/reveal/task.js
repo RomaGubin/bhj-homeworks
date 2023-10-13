6fffd8823da9ac93803af656ad8b19c624d474a4
Roman G <roma.gubin2011@yandex.ru>
2023-10-13 23:16:54 +0500

    fourNew commit

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,14 @@
 const reveal = document.querySelectorAll('.reveal');
 
 reveal.forEach( function(reveal) {
+  
   setInterval(() => {
     const { top, bottom } = reveal.getBoundingClientRect()
 
     if (bottom < 0 || top > window.innerHeight) {
       return reveal.classList.remove('reveal_active')
     }
-    
+
     return reveal.classList.add('reveal_active')
   }, 500);
 });
